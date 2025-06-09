67339472701d9788468c65d504628baf95565d17
Matheus Francisco <matheusfrancisco@ciandt.com>
2025-06-09 15:45:13 -0300

    feat: Implement CRUD endpoints for Person entity
                  Has a CRUD (Create, Read, Update, Delete) operations for the Person entity.
    
                  It adds the following REST endpoints to the PersonController:
    
                  POST /create: Creates a new person.
                  GET /findAll: Retrieves a list of all persons.
                  PUT /update: Updates an existing person.
                  DELETE /{id}: Deletes a person by their ID.
                  Additionally, it includes the initial service layer implementation with mock data for the findAll method to support testing and development.

--- projects-spring/src/main/java/study/projects_spring/firstendpoint/controller/PersonController.java
+++ projects-spring/src/main/java/study/projects_spring/firstendpoint/controller/PersonController.java
@@ -2,12 +2,11 @@ package study.projects_spring.firstendpoint.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import study.projects_spring.firstendpoint.model.Person;
 import study.projects_spring.firstendpoint.service.PersonService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
@@ -21,6 +20,33 @@ public class PersonController {
     produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable("id") String id){
      return   service.findById(id);
+    }
+    @RequestMapping(value = "create",
+    method = RequestMethod.POST,
+    produces = MediaType.APPLICATION_JSON_VALUE,
+    consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person){
+     return   service.create(person);
+    }
+   @RequestMapping(value = "update",
+    method = RequestMethod.PUT,
+    produces = MediaType.APPLICATION_JSON_VALUE,
+    consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person){
+     return   service.update(person);
+    }
+
+    @RequestMapping(value = "/findAll",
+    method = RequestMethod.GET,
+    produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll( ){
+     return   service.findAll();
+    }
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id){
+        service.delete(id);
     }
 }
 
--- projects-spring/src/main/java/study/projects_spring/firstendpoint/service/PersonService.java
+++ projects-spring/src/main/java/study/projects_spring/firstendpoint/service/PersonService.java
@@ -3,6 +3,8 @@ package study.projects_spring.firstendpoint.service;
 import org.springframework.stereotype.Service;
 import study.projects_spring.firstendpoint.model.Person;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -23,7 +25,39 @@ public class PersonService {
        return person;
    }
 
+   public List<Person> findAll(){
+       logger.info("Finding all Person!");
+       List<Person> persons = new ArrayList<Person>();
+       for (int i = 0; i<8;i++){
+           Person person = mockPerson(i);
+           persons.add(person);
+       }
+       return persons;
+   }
 
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Firstname"+i);
+        person.setLastName("LastName"+i);
+        person.setAddress("Sao paulo - Brazil");
+        person.setGender("Male");
+        return person;
+    }
+    public Person create(Person person){
+        logger.info("Creating a new Person!");
 
+        return person;
+    }
+    public Person update(Person person){
+        logger.info("Updating a Person!");
+
+        return person;
+    }
+
+    public void delete(String id){
+        logger.info("Deleting one Person!");
+
+    }
 
 }
